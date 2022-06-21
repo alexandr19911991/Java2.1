bdf3e1c06bab6610064c87a8c446d80c11b89272
Alexandr19911991 <swiper1991@mail.ru>
2022-06-21 16:57:48 +0300

    Bonus

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amountInKopecks = 1000;
+        int percentInKopecks = 20;
+        int bonus = amountInKopecks / percentInKopecks;
+        System.out.println("Здравствуйте Акакий! Ваш бонус равен " + bonus + " миль. Спасибо что пользуетесь нашими авиалиниями.");
+    }
+}
